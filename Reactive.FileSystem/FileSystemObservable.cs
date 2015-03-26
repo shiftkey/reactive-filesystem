f8f6e5cff0942389f3ae2c0da2016c3786e75815
Brendan Forster <brendan@github.com>
2015-03-26 09:15:32 +0100

    stubbing a simple example

---
+++ Reactive.FileSystem/FileSystemObservable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reactive.Linq;
+
+namespace Reactive.FileSystem
+{
+    public class FileSystemObservable : IDisposable
+    {
+        readonly FileSystemWatcher watcher;
+
+        public FileSystemObservable(string path)
+        {
+            watcher = new FileSystemWatcher(path);
+        }
+
+        public IObservable<string> Changed
+        {
+            get
+            {
+                var obs = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(
+                h => watcher.Changed += h,
+                h => watcher.Changed -= h);
+
+                return obs.Select(x => x.EventArgs.FullPath);
+            }
+        }
+
+        bool disposed = false;
+        public void Dispose()
+        {
+            if (disposed) return;
+            
+            watcher.Dispose();
+            disposed = true;
+        }
+    }
+}
